8888b20b7bc8fe8fb3e7397f0b940227374345c3
Reyden <martin.leblancs@epitech.eu>
2023-03-07 17:17:45 +0100

    [ADD] Test reponsive

--- R-type Site/src/mainPart.js
+++ R-type Site/src/mainPart.js
@@ -7,79 +7,19 @@ import { Grid } from "@mui/material";
 
 function MainPart() {
     return (
-            <Grid container>
-                <Grid item>
-                    <img
-                        src="https://images4.alphacoders.com/248/248634.jpg"
-                        style={{ width: "100%" }}
-                        alt={"Background"}
-                    />
-                </Grid>
-                <Grid item>
-                    <Typography
-                        variant="h2"
-                        component="h1"
-                        sx={{
-                            color: "white",
-                            position: "absolute",
-                            top: "20%",
-                            left: "10%",
-                            textShadow: "0 0 10px black",
-                            fontFamily: "R-Type",
-                        }}
-                    >
-                        R-Type The Game
-                    </Typography>
-                </Grid>
-                <Grid item>
-                <Typography
-                        variant="h3"
-                        component="h1"
-                        sx={{
-                            color: "white",
-                            position: "absolute",
-                            top: "30%",
-                            left: "10%",
-                            textShadow: "0 0 10px black",
-                            fontFamily: "R-Type",
-                        }}
-                    >
-                        The ultimate space battle starts here
-                    </Typography>
-                </Grid>
-                    <Grid container>
-                        <Grid item>
-                        <Typography
-                            variant="h3"
-                            component="h1"
-                            sx={{
-                                color: "white",
-                                position: "absolute",
-                                top: "70%",
-                                left: "42%",
-                                textShadow: "0 0 10px black",
-                                fontFamily: "R-Type",
-                            }}
-                        >
-                            Available on :
-                        </Typography>
-                        </Grid>
-                        <Grid item>
-                    <img
-                        src={linux}
-                        style={{ position: "absolute", width: '10%', top: '80%', left: '40%' }}
-                        alt={"Image1"}
-                    />
-                        </Grid>
-                        <Grid item>
-                    <img
-                        src={windows}
-                        style={{ position: "absolute", width: '8%', top: '82%', left: '53%' }}
-                        alt={"Image2"}
-                    />
-                        </Grid>
-                    </Grid>
+        <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+                <TextField label="Nom" variant="outlined" />
             </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField label="Adresse" variant="outlined" />
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant="contained" color="primary">
+                    Enregistrer
+                </Button>
+            </Grid>
+        </Grid>
     );
 }
 
